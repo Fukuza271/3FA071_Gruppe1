5c4bdb40770ae5599029ef9a1fb3fc513d0614a2
Julian <juli_krause@gmx.de>
2024-10-11 10:32:42 +0200

    Properties File angepasst

---
+++ src/main/java/Property.java
@@ -0,0 +1,35 @@
+import Konstanten.Constants;
+import database.DatabaseConnection;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Property {
+
+    public static java.util.Properties readProperties() {
+        // Systemnutzer auslesen
+        String systemnutzer = System.getProperty("user.name");
+        // Home-Verzeichnis des Nutzers
+        String homeDir = System.getProperty("user.dir");
+        String fileName = "database.properties";
+        String filePath = homeDir + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + fileName;
+        Properties properties = new Properties();
+        try {
+            FileInputStream input = new FileInputStream(filePath);
+            properties.load(input);
+            // Werte basierend auf dem Systemnutzer laden
+            String dbUrl = properties.getProperty("systemnutzer.db.url");
+            String dbUser = properties.getProperty("systemnutzer.db.user");
+            String dbPassword = properties.getProperty("systemnutzer.db.pw");
+            System.out.println("Datenbank-URL: " + dbUrl);
+            System.out.println("Benutzer: " + dbUser);
+            System.out.println("Passwort: " + dbPassword);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
