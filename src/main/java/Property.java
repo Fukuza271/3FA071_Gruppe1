e265e9bbb282c4029a6f4393d7d3ec053f3fcc86
Julian <juli_krause@gmx.de>
2024-10-11 11:19:40 +0200

    Property an Systemuser angepasst

--- src/main/java/Property.java
+++ src/main/java/Property.java
@@ -8,7 +8,6 @@ public class Property {
     public static java.util.Properties readProperties() {
         // Systemnutzer auslesen
         String systemnutzer = System.getProperty("user.name");
-
         // Home-Verzeichnis des Nutzers
         String homeDir = System.getProperty("user.dir");
         String fileName = "database.properties";
@@ -17,15 +16,13 @@ public class Property {
         try {
             FileInputStream input = new FileInputStream(filePath);
             properties.load(input);
-
             // Werte basierend auf dem Systemnutzer laden
-            String dbUrl = properties.getProperty("systemnutzer.db.url");
-            String dbUser = properties.getProperty("systemnutzer.db.user");
-            String dbPassword = properties.getProperty("systemnutzer.db.pw");
+            String dbUrl = properties.getProperty(systemnutzer + ".db.url");
+            String dbUser = properties.getProperty(systemnutzer + ".db.user");
+            String dbPassword = properties.getProperty(systemnutzer + ".db.pw");
             System.out.println("Datenbank-URL: " + dbUrl);
             System.out.println("Benutzer: " + dbUser);
             System.out.println("Passwort: " + dbPassword);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
