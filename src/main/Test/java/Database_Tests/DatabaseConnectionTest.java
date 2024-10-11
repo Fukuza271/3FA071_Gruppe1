d181e2de8bb4fb8cbe3af738b928a03404a6b93c
Julian <juli_krause@gmx.de>
2024-10-11 13:12:00 +0200

    Connection Tests angefangen

---
+++ src/main/Test/java/Database_Tests/DatabaseConnectionTest.java
@@ -0,0 +1,26 @@
+package Database_Tests;
+
+import database.DatabaseConnection;
+import database.Property;
+import interfaces.IDatabaseConnection;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class DatabaseConnectionTest {
+
+    @Test
+    public void testOpenConnection() throws SQLException {
+        IDatabaseConnection connection = new DatabaseConnection();
+        Connection mockConnection = Mockito.mock(Connection.class);
+        Mockito.when(DriverManager.getConnection(Mockito.anyString(), Mockito.anyString(), Mockito.anyString())).thenReturn(mockConnection);
+        IDatabaseConnection databaseConnection = connection.openConnection(Property.readProperties());
+        Assert.assertNotNull(databaseConnection);
+    }
+}
