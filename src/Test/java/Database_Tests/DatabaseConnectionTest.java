32a21e2e5b9ce38af8e2bb3b53e5f2a684e0d6d6
Knowiii <annabel.amon@gmail.com>
2024-10-16 08:02:28 +0200

    added Test truncateAllTables

--- src/Test/java/Database_Tests/DatabaseConnectionTest.java
+++ src/Test/java/Database_Tests/DatabaseConnectionTest.java
@@ -2,6 +2,7 @@ package Database_Tests;
 
 import database.DatabaseConnection;
 import database.Property;
+import database.daos.CustomerDao;
 import interfaces.IDatabaseConnection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,24 @@ public class DatabaseConnectionTest {
         IDatabaseConnection result = connection.openConnection(Property.readProperties());
         Assertions.assertNotNull(result);
         connection.closeConnection();
+    }
+
+
+
+
+    @Test
+    public void truncateAllTablesTest() {
+        DatabaseConnection dbCon = new DatabaseConnection();
+        CustomerDao custDao = new CustomerDao();
+
+        TestData.insertCustomerTestData();
+        System.out.println("Listsize: " + custDao.findAll().size());
+        Assertions.assertEquals(custDao.findAll().size(), 10);
+
+        dbCon.truncateAllTables();
+        System.out.println("Listsize: " + custDao.findAll().size());
+        Assertions.assertEquals(custDao.findAll().size(), 0);
+
+
     }
 }
