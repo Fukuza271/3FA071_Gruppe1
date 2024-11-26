e0532c856ad33c0126aa0659950ff320cd5f973c
Paul Wild <paul.wild01@gmx.de>
2024-11-26 11:41:13 +0100

    add endpoint setupDB

---
+++ src/main/java/rest/DatabaseSetup.java
@@ -0,0 +1,22 @@
+package rest;
+
+import database.DatabaseConnection;
+import database.ExampleDataProvider;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("setupDB")
+public class DatabaseSetup {
+    @DELETE
+    public Response DbSetup() {
+        (new DatabaseConnection()).createAllTables();
+
+        ExampleDataProvider.shared.insertCustomerData();
+        ExampleDataProvider.shared.insertHeizungReadingData();
+        ExampleDataProvider.shared.insertWasserReadingData();
+        ExampleDataProvider.shared.insertStromReadingData();
+
+        return Response.status(Response.Status.OK).build();
+    }
+}
