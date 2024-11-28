67e7af09d0bf5e13430092eba0e5d9f4e2179847
Knowiii <annabel.amon@gmail.com>
2024-11-28 15:16:03 +0100

    changed ExampleDataProviderTest

--- src/test/java/Database_Tests/ExampleDataProviderTest.java
+++ src/test/java/Database_Tests/ExampleDataProviderTest.java
@@ -1,36 +1,85 @@
 package Database_Tests;
 
 import database.ExampleDataProvider;
+import database.entities.Customer;
+import database.entities.Reading;
+import interfaces.ICustomer;
+import interfaces.IReading;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ExampleDataProviderTest {
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class ExampleDataProviderTest extends BasicTests {
+
+    @BeforeEach
+    public void setUp(){
+        BasicTests.connection.removeAllTables();
+        BasicTests.connection.createAllTables();
+    }
+
+    String testCustomerUUID = "ec617965-88b4-4721-8158-ee36c38e4db3";
+
+    public void insertTestCustomer() {
+        customerDao.insert(new Customer(UUID.fromString(testCustomerUUID),
+                ICustomer.Gender.M, "Pumukel", "Kobold", LocalDate.now()));
+    }
 
     @Test
     public void insertHeizungReadingDataTest() {
-        ExampleDataProvider.shared.insertHeizungReadingData();
 
-        System.out.println("Reading added");
+        Assertions.assertEquals(0, readingDao.findAll().size());
+
+        insertTestCustomer();
+
+        readingDao.insert(new Reading(UUID.randomUUID(),
+                UUID.fromString(testCustomerUUID), LocalDate.now(),
+                "H-2010-2312456ab", 5.965, IReading.KindOfMeter.HEIZUNG,
+                "TestReading", false));
+
+        Assertions.assertEquals(1, readingDao.findAll().size());
     }
 
     @Test
     public void insertWasserReadingDataTest() {
-        ExampleDataProvider.shared.insertWasserReadingData();
 
-        System.out.println("Reading added");
+        Assertions.assertEquals(0, readingDao.findAll().size());
+
+        insertTestCustomer();
+
+        readingDao.insert(new Reading(UUID.randomUUID(),
+                UUID.fromString(testCustomerUUID), LocalDate.now(),
+                "S-2011-2312456cd", 16573.d, IReading.KindOfMeter.HEIZUNG,
+                "TestReading", false));
+
+        Assertions.assertEquals(1, readingDao.findAll().size());
     }
 
     @Test
     public void insertStromReadingDataTest() {
-        ExampleDataProvider.shared.insertStromReadingData();
 
-        System.out.println("Reading added");
+        Assertions.assertEquals(0, readingDao.findAll().size());
+
+        insertTestCustomer();
+
+        readingDao.insert(new Reading(UUID.randomUUID(),
+                UUID.fromString("ec617965-88b4-4721-8158-ee36c38e4db3"), LocalDate.now(),
+                "W-2012-2312456ef", 710.d, IReading.KindOfMeter.HEIZUNG,
+                "TestReading", false));
+
+        Assertions.assertEquals(1, readingDao.findAll().size());
     }
 
     @Test
     public void insertCustomerData() {
+
+        Assertions.assertEquals(0, customerDao.findAll().size());
+
         ExampleDataProvider.shared.insertCustomerData();
 
-        System.out.println("Customer added");
+        Assertions.assertEquals(1001, customerDao.findAll().size());
     }
 
 }
