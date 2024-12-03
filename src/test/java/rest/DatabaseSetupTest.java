5852a62f8281e6819618448f777940a3ebcf9194
Knowiii <annabel.amon@gmail.com>
2024-12-03 13:27:19 +0100

    changed Rest_Tests to rest

--- src/test/java/rest/DatabaseSetupTest.java
+++ src/test/java/rest/DatabaseSetupTest.java
@@ -1,12 +1,10 @@
-package Rest_Tests;
+package rest;
 
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mariadb.jdbc.client.Client;
 
 public class DatabaseSetupTest {
 
