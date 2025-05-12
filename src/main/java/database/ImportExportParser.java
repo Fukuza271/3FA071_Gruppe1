fd11fbbdbe7ba3abd708e12f5fb069012c695889
Julian <juli_krause@gmx.de>
2025-05-12 10:21:15 +0200

    CLI Zwischenstand

--- src/main/java/database/ImportExportParser.java
+++ src/main/java/database/ImportExportParser.java
@@ -2,10 +2,15 @@ package database;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import database.daos.CustomerDao;
 import database.entities.Customer;
+import org.checkerframework.checker.units.qual.C;
+import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImportExportParser {
@@ -17,7 +22,14 @@ public class ImportExportParser {
     }
 
     public static void exportJSONCustomer() {
-
+        CustomerDao customerDao = new CustomerDao();
+        List<Customer> customers = customerDao.findAll();
+        Gson gson = new Gson();
+        List<String> formatList = new ArrayList<>();
+        for (Customer customer: customers) {
+            formatList.add(gson.toJson(customer));
+        }
+        @CommandLine.Command()
     }
 
     public static void exportJSONReading() {
